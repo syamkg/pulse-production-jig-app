0b2d22aff8d7979a93a10756db3e418f89b7725f
Gareth Evans <evans.g.p@gmail.com>
2022-04-14 14:22:33 +0800

    add configuration to project

---
+++ pulse_jig/config.py
@@ -0,0 +1,14 @@
+from dynaconf import Dynaconf, Validator
+
+settings = Dynaconf(
+    envvar_prefix="JIG",
+    settings_files=["settings.yaml", ".secrets.yaml"],
+    validator=[
+        Validator("lora.join_eui", must_exist=True),
+        # should not be present outside of development as it will be generated uniquely for each device
+        Validator("lora.app_key", must_exist=None),
+    ],
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
