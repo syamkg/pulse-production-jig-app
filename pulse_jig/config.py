7b39055cf68fc7bd8de8567aaf8215ae451fc5c6
Deshan Dissanayake <deshan@kzn.io>
2022-08-18 13:15:10 +0800

    More validators added to verify settings file (#61) #minor

--- pulse_jig/config.py
+++ pulse_jig/config.py
@@ -5,7 +5,15 @@ settings = Dynaconf(
     settings_files=["settings.yaml", ".secrets.yaml"],
     validators=[
         Validator("app.target", must_exist=True, is_in=["pulse-r1b", "ta3k", "fake"]),
-        Validator("app.debug", "app.test_firmware_path", must_exist=True),
+        Validator(
+            "app.debug", "app.test_firmware_path", "app.skip_firmware_load", "app.hwspec_repair_mode", must_exist=True
+        ),
+        Validator("app.prod_firmware_path", must_exist=True, when=Validator("app.target", eq="pulse-r1b")),
+        Validator(
+            "app.hwspec_repair_mode",
+            eq=False,
+            when=Validator("app.target", eq="pulse-r1b") & Validator("mode_vars.pulse_test_phase", eq=2),
+        ),
         Validator(
             "device.minter_id",
             "device.thing_type_name",
@@ -19,6 +27,7 @@ settings = Dynaconf(
             must_exist=True,
         ),
         Validator("mode_vars", must_exist=True),
+        Validator("mode_vars.cable_length", must_exist=True, when=Validator("app.target", eq="ta3k")),
         Validator("api.region", "api.host", "api.stage", must_exist=True),
         Validator("lora.test.join_eui", "lora.test.app_key", "lora.config.join_eui", must_exist=True),
         Validator("network.ping_interval", must_exist=True),
