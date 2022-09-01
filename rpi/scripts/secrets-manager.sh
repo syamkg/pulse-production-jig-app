42ea940ce06d346c8ce841db1d7e9834cb7a64c6
Deshan Dissanayake <deshan@kzn.io>
2022-09-01 16:32:45 +0800

    Script updated to match the "secret" structure (#78)
    * Script updated to match the "secret" structure
    
    * Devcode removed

--- rpi/scripts/secrets-manager.sh
+++ rpi/scripts/secrets-manager.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+set -eu
+
+# Get all instances with the given tag
+instances="$( aws ssm describe-instance-information --filters "Key=tag:instance_tag,Values=pulse-jig" )"
+
+# Parse the above output & get the InstanceId
+node_id="$( echo $instances | jq -r --arg name "${1}" '.InstanceInformationList | .[] | select( .Name == $name ) | .InstanceId' )"
+
+# Create the secret
 aws secretsmanager create-secret \
-    --name "$1" \
+    --name "${1}" \
     --description "Raspberry Pi login credentials" \
-    --secret-string "{\"user\":\"pi\",\"password\":\"$2\",\"node_name\":\"$1\"}"
+    --secret-string "{\"user\":\"pi\",\"password\":\"${2}\",\"node_name\":\"${1}\",\"node_id\":\"${node_id}\"}"
